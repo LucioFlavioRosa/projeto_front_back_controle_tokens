299233354de180c30430012426f3fb93d128e758
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-11-03 23:02:56 -0300

    lpeza

--- backend/app/api/v1/api.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from .endpoints import token_analytics
-
-api_router = APIRouter()
-api_router.include_router(token_analytics.router, prefix="", tags=["Token Analytics"])
