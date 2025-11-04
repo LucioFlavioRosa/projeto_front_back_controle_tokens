27f4ee5438b74508157f8265ff0d8f93c441d9d0
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-11-03 22:19:24 -0300

    feat: backend/run.py

---
+++ backend/run.py
@@ -0,0 +1,9 @@
+import os
+from app import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    env = os.environ.get('FLASK_ENV', 'production')
+    debug = env == 'development'
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=debug)
